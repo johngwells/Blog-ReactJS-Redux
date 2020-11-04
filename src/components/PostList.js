e99cc808ed7daddf90eece49775393c7117414d6
John Wells <molekewl2017@gmail.com>
2020-11-03 19:14:38 -0800

    add postsReducer and connect to postList to display blog data

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import postsReducer from '../reducers/postsReducer';
 
 class PostList extends React.Component {
   componentDidMount() {
@@ -8,8 +9,13 @@ class PostList extends React.Component {
   }
 
   render() {
+    console.log(this.props.posts);
     return <div>Post List</div>
   }
 }
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
